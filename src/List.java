26c2dca3de6f2d52ea95f972cc2c9c51a71ca7ef
kelly lam <kellylhw@yahoo.com.hk>
2014-11-15 15:37:08 -0800

    Create class List and class Person

---
+++ src/List.java
@@ -0,0 +1,37 @@
+/*
+ * Define a reusable class List.
+ * It allows the user to select the view they want to do search/sort
+ * and returns these results in array list.
+ */
+public class List {
+	private final int LIST_MAX_LENGTH = 100;			//its value can be changed
+
+	/**
+	 * It is a constructor that creates an array list
+	 */
+	public void construct() {
+		
+	}
+	
+	/**
+	 * Set the value of the field being searched to "newSearchField"
+	 */
+	public void searchByField(String newSearchField) {
+		String searchField;
+	}
+	
+	/**
+	 * Set the value of the field being sorted to "newSortField"
+	 */
+	public void sortByField(String newSortField) {
+		String sortField;
+	}
+	
+	/**
+	 * Return string if no search is matched
+	 */
+	public String NoSearchMatch() {
+		
+	}
+	
+}
