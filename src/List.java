01a2e6d5418e8e30b76a1d8f84873890ef922945
Shmuel Shaffer <shmuel.shaffer@gmail.com>
2014-11-16 16:19:02 -0800

    Disk Skeleton plus the List and Person stub classes to make Disk compile

---
+++ src/List.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.*;
+
+
+
+/**
+ * This is a dummy class that I need to build my skeleton classes.
+ * It will be replaced by the actual Person class once it is available.
+
+ * @author Shmuel Shaffer
+ */
+public class List {
+
+	ArrayList <Person> list = new ArrayList <Person>();
+	// Person contact1 =new Person("Foe", "Smith") ;
+
+	// list.add(contact1);
+	
+//	  ArrayList<String> obj = new ArrayList<String>();
+	  public void addContact(Person newContact) {
+	  /*This is how elements should be added to the array list*/
+	  list.add(newContact);
+
+
+}
+}
---
+++ src/Person.java
@@ -0,0 +1,21 @@
+/**
+ * This is a dummy class that I need to build my skeleton classes.
+ * It will be replaced by the actual Person class once it is avaialble.
+
+ * @author Shmuel Shaffer
+ */
+public class Person {
+
+	/**
+	 * Constructor for the Disk Class finds if the DirectoryProject.txt file
+	 * exists. If it does not exists, the constructor created the file and
+	 * closes it
+	 */
+	private String firstName ;
+	private String LastName;
+	Person(String first, String last){
+		firstName = first;
+		LastName = last;
+	}
+
+}
