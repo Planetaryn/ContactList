8dbc1005d25d9698954b4b80a63a5f9e8bc05f1b
kelly lam <kellylhw@yahoo.com.hk>
2014-11-17 18:48:08 -0800

    Updated

--- src/ContactList.java
+++ src/ContactList.java
@@ -1,37 +1,66 @@
-/*
- * Define a reusable class List.
- * It allows the user to select the view they want to do search/sort
- * and returns these results in array list
+import java.util.*;
+
+/**
+ * Define a reusable class List. It allows the user to select the view they want
+ * to do search/sort and returns these results in array list
+ * File History:
+ * Author: Kelly Lam version 1
+ * Version 2: Noah
  */
-public class List {
-	private final int LIST_MAX_LENGTH = 100;			//its value can be changed
+public class ContactList {
+	private final int LIST_MAX_LENGTH = 100;
 
 	/**
-	 * It is a constructor that creates an array list
+	 * Constructs a new object of class ContactList of length contactListLength
+	 * 
+	 * @param contactListLength
 	 */
-	public void construct() {
-		
+	public ContactList(int contactListLength) {
+
 	}
-	
+
 	/**
-	 * Set the value of the field being searched
+	 * Sets the value of searchField to the value of the parameter newSearchField, and returns the array indices of the person objects which match the search
+	 * @param newSearchField
+	 * @return
 	 */
-	public void searchByField(String newSearchField) {
+	public int[] searchByField(String newSearchField) {
 		String searchField;
+		System.out.println("Method ContactList.searchByField called");
+		int[] placeHolder = {1,2,3};
+		return placeHolder;
 	}
-	
+
 	/**
-	 * Set the value of the field being sorted
+	 * Sets the value of sortField to the value of the parameter newSortField, and sorts the ContactList by sortField
+	 * @param newSortField
+	 * @return
 	 */
+
 	public void sortByField(String newSortField) {
 		String sortField;
+		System.out.println("Method ContactList.sortByField called");
+	}
+
+	/**
+	 * Returns true if the search has no matches.
+	 */
+	public boolean noSearchMatch() {
+		return true;
 	}
 	
 	/**
-	 * Return value if no search is matched
+	 * Call the programme to create a new Person
 	 */
-	public String NoSearchMatch() {
-		
+	public void newPerson() {
+		System.out.println("Using method newPerson");	
 	}
 	
+	/**
+	 * Return the information of the Contact List
+	 */
+	public String toString() {
+		System.out.println("Using method toString");
+		return ("");
+	}
 }
--- src/Person.java
+++ src/Person.java
@@ -1,14 +1,19 @@
 /*
  * Define a reusable class Person. One object of class Person includes email, 
  * phone number, notes.
+ * File History:
+ * Author Kelly Lam Version 1
+ * Version 2: Irma added variables and methods to handle first and last name
  */
 public class Person {
 	private String vizEmail;
-	private String vizPhoneNumber;
+	private int vizPhoneNumber;
 	private String vizNotes;
-	private String ssEmail;
-	private String ssPhoneNumber;
-	private String ssNotes;
+	private String normalizedEmail;
+	private int normalizedPhoneNumber;
+	private String normalizedNotes;
+	private String firstName;
+	private String lastName;
 	
 	/**
 	 * It is a parameterized constructor that runs Name, StreetAddress and Notes.
@@ -21,107 +26,159 @@ public class Person {
 	 * Set the value for visible email
 	 */
 	public void setVizEmail (String newVizEmail) {
-		
+		System.out.println("Using method setVizEmail");
 	}
 	
 	/**
 	 * Set the value for visible phone number
 	 */
-	public void setVizPhoneNumber(String newVizPhoneNumber) {
-		
+	public void setVizPhoneNumber(int newVizPhoneNumber) {
+		System.out.println("Using method setVizPhoneNumber");
 	}
 
 	/**
 	 * Set the value for visible notes
 	 */
 	public void setVizNotes(String newVizNotes) {
-		
+		System.out.println("Using method setVizNotes");	
 	}
 	
 	/**
 	 * Return the value of visible email
 	 */
 	public String getVizEmail() {
-		
+		System.out.println("Using method getVizEmail");
+		return vizEmail;
 	}
 	
 	/**
 	 * Return the value of visible phone number
 	 */
 	public int getVizPhoneNumber() {
-		
+		System.out.println("Using method getVizPhoneNumber");
+		return vizPhoneNumber;
 	}
 	
 	/**
 	 * Return the value of visible notes
 	 */
 	public String getVizNotes() {
-		
+		System.out.println("Using method getVizNotes");
+		return vizNotes;
 	}
 	
 	/**
-	 * Set the value for search/sort email
+	 * Set the value for normalized email
 	 */
-	public void setSSEmail (String newSSEmail) {
-		
+	public void setNormalizedEmail (String newNormailizedEmail) {
+		System.out.println("Using method setNormalizedEmail");
 	}
 	
 	/**
-	 * Set the value for search/sort phone number
+	 * Set the value for normalized phone number
 	 */
-	public void setSSPhoneNumber(String newSSPhoneNumber) {
-		
+	public void setNormalizedPhoneNumber(int newNormalizedPhoneNumber) {
+		System.out.println("Using method setNormalizedPhoneNumber");
 	}
 
 	/**
-	 * Set the value for search/sort notes to
+	 * Set the value for normalized notes to
 	 */
-	public void setSSNotes(String newSSNotes) {
-		
+	public void setNormalizedNotes(String newNormailzedNotes) {
+		System.out.println("Using method setNormalizedNotes");
 	}
 	
 	/**
-	 * Return the result that search/sort by email
+	 * Return the result of normalized email
 	 */
-	public String getSSEmail() {
-		
+	public String getNormalizedEmail() {
+		System.out.println("Using method getNormalizedEmail");
+		return normalizedEmail;
 	}
 	
 	/**
-	 * Return the result that search/sort by phone number
+	 * Return the result of normalized phone number
 	 */
-	public int getSSPhoneNumber() {
-		
+	public int getNormalizedPhoneNumber() {
+		System.out.println("Using method getNormalizedPhoneNumber");
+		return normalizedPhoneNumber;
 	}
 	
 	/**
-	 * Return the result that search/sort by notes
+	 * Return the result normalized notes
 	 */
-	public String getSSNotes() {
-		
+	public String getNormlizedNotes() {
+		System.out.println("Using method getNormalizedNotes");
+		return normalizedNotes;
 	}
 
 	/**
-	 * Return all the data of the people
+	 * Return all the information of the contact list
 	 */
 	public String toString() {
-		
+		System.out.println("Using method toSring");
+		return vizEmail + vizPhoneNumber + vizNotes;
 	}
+	
+    /**
+     * Sets the value for firstName to "first"
+     * @param first
+     */
+    public void setFirstName(String first) {
+        System.out.println("Using method setFirstName ");
+    }
+
+    /**
+     * Sets the value for firstName to "last"
+     * @param last
+     */
+
+    public void setLastName(String last) {
+        System.out.println("Using method setLastName ");
+    }
+
+    /**
+     * Returns the value in firstName
+     * @return
+     */
+    public String getFirsName() {
+        System.out.println("Using method getFirstName ");
+        return firstName;
+    }
+
+    /**
+     * Returns the value in lastName
+     * @return
+     */
+    public String getLastName() {
+        System.out.println("Using method getLastName ");
+        return lastName;
+    }
+
+    /**
+     * Prints the values for lastNmae and firstName fo the Name that calls this method.
+     */
+    public void printName() {
+        System.out.println("Using method printName ");
+    }
 	
 	//The below two methods is in response to case 1A
 	/**
 	 * Verify the phone number format to make sure the phone number is valid
 	 * It does no return to user
 	 */
-	public void verifyPhoneFormat(String newVizPhoneNumber) {
-		
+	public boolean verifyPhoneFormat(String newVizPhoneNumber) {
+		System.out.println("Using method verifyPhoneFormat");
+		return true;
 	}
 
 	/**
 	 * Verify the email format to make sure the email is valid
 	 * It will prompt users to correct it if the email is invalid
 	 */
-	public String verifyEmailFormat(String newVizEmail) {
-		
+	public boolean verifyEmailFormat(String newVizEmail) {
+		System.out.println("Using method verifyEmailFormat");
+		return true;
 	}
+
 }
