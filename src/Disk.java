a1456ee499c08fe7fd0fbc97a59bbc0e7392f445
Shmuel Shaffer <shmuel.shaffer@gmail.com>
2014-11-22 09:16:18 -0800

    1. The new readFromDisk method only reads the disk and stores the read object in a private class variable. The method returns true if the read was successful (file exists, etc.) or false if the read operation failed. 2. In order to get the Contact list, main needs to invoke a new method getObjectFromDiskFile. 3. This can be done in a single operation if (readFromDisk)    contactList = getObjectFromDiskFile();

--- src/Disk.java
+++ src/Disk.java
@@ -7,7 +7,7 @@
  * @author Shmuel Shaffer
  * This is a version 2 which includes modifications based on feedback we received in the class.
  * Version 3: changed the functionality of the contractor to modify the private fileExists flag
- * formated V3:
+ * formated V4: Added a getDiskFileObject method
  */
 import java.io.*;
 import java.util.*;
@@ -15,6 +15,7 @@ import java.util.*;
 public class Disk {
 	private final String CONTACT_LIST_FILE_NAME = "Contact_List.ser";
 	private boolean fileExists;
+	private ContactList directoryOfContactsFromDisk;
 
 	/**
 	 * Constructor for the Disk Class finds if the DirectoryProject.txt file
@@ -37,16 +38,27 @@ public class Disk {
 	}
 
 	/**
-	 * The writeToDisk method gets the ContactList Object as an input parameter.
-	 * The method attempts to open a directory file on the disk. If the file
-	 * does not exist the method returns false. If the file exists, the method
-	 * returns true and populates the ContactList object with the directory
-	 * object from the disk file.
+	 * The readFromDisk method attempts to read an object from the disk. If the read
+	 * operation fails, the method returns false. If the read operation succeeds
+	 * the method returns true and populates the private class variable ContactList 
+	 * object with the directory object from the disk file.
 	 */
-	public boolean readFromDisk(ContactList directoryOfContacts) {
+
+	public boolean readFromDisk() {
 		boolean successfulyreadFromDisk = true;
 		System.out.println("Method readFromDisk called");
 		return successfulyreadFromDisk;
 	}
+	
+	/**
+	 * The method getObjectFromDiskFile returns the object which was read 
+	 * from the disk by the readFromDisk method.
+	 * If the readFromDisk method was not successful, the method returns 
+	 * null (rather than the address of a valid ContactList object).
+	 */
+	public ContactList  getDiskFileObject(){
+		System.out.println("Method getDiskFileObject called");
+		return directoryOfContactsFromDisk;
+}
 
 }
