2a380c8b7e761934d7201b5b01935f0234cc8aac
Shmuel Shaffer <shmuel.shaffer@gmail.com>
2014-11-22 17:15:19 -0800

    Added @ param to the java doc of the methods

--- src/Disk.java
+++ src/Disk.java
@@ -7,7 +7,10 @@
  * @author Shmuel Shaffer
  * This is a version 2 which includes modifications based on feedback we received in the class.
  * Version 3: changed the functionality of the contractor to modify the private fileExists flag
- * formated V4: Added a getDiskFileObject method
+ * formated 
+ * V4: Added a getDiskFileObject method
+ * V5: Added @param to the java doc description
+ * 
  */
 import java.io.*;
 import java.util.*;
@@ -21,6 +24,7 @@ public class Disk {
 	 * Constructor for the Disk Class finds if the DirectoryProject.txt file
 	 * exists. If it does not exists, the constructor sets the value of the
 	 * fileExists variable to true.
+	 *  @param 
 	 */
 	Disk() {
 
@@ -30,6 +34,9 @@ public class Disk {
 	 * The writeToDisk method gets the ContactList Object as an input parameter
 	 * and overrides the existing directory file with a new directory file. The
 	 * method returns true if successful, and false if an error occurs.
+	 * 
+	 *  @param  - directoryOfContacts: ContactList object containing the directory object
+	 *  @return - Returns true if write operation was successful and false if it failed.
 	 */
 	public boolean writeToDisk(ContactList directoryOfContacts) {
 		boolean successfulyWrittenToDisk = true;
@@ -42,6 +49,8 @@ public class Disk {
 	 * operation fails, the method returns false. If the read operation succeeds
 	 * the method returns true and populates the private class variable ContactList 
 	 * object with the directory object from the disk file.
+	 * 	  
+	 *  @return - Returns true if read is successful and false if operation failed.
 	 */
 
 	public boolean readFromDisk() {
@@ -55,6 +64,9 @@ public class Disk {
 	 * from the disk by the readFromDisk method.
 	 * If the readFromDisk method was not successful, the method returns 
 	 * null (rather than the address of a valid ContactList object).
+	 * 
+	 *  @return - Returns ContactList object containing the directory object
+	 *  		  which was read from the disk.
 	 */
 	public ContactList  getDiskFileObject(){
 		System.out.println("Method getDiskFileObject called");
